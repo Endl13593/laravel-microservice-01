3511fcae396e03cab5260db012c3881d4ea9e0ed
eduardo <nunes.eduardo1993@gmail.com>
2021-07-07 18:05:19 -0400

    Created Model and Migration Company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'uuid',
+        'name',
+        'url',
+        'phone',
+        'whatsapp',
+        'email',
+        'facebook',
+        'instagram',
+        'youtube',
+        'image',
+    ];
 }
